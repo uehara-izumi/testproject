cbf95b1bdc419a8221d0412273cc516844da51d8
Izumin <60781486+uehara-izumi@users.noreply.github.com>
2022-05-15 22:00:56 +0900

    Add files via upload
    db

--- testproject/public/index.php
+++ testproject/public/index.php
@@ -1,55 +1,34 @@
-<?php
+<html>
+<main>
+    <h2>登録した部品名一覧</h2>
+    
+    <?php
+        try{
+            
+	    $dbhost = $_SERVER['RDS_HOSTNAME'];
+        $dbport = $_SERVER['RDS_PORT'];
+        $dbname = $_SERVER['RDS_DB_NAME'];
+        $charset = 'utf8' ;
 
-use Illuminate\Contracts\Http\Kernel;
-use Illuminate\Http\Request;
+        $dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
+        $username = $_SERVER['RDS_USERNAME'];
+        $password = $_SERVER['RDS_PASSWORD'];
 
-define('LARAVEL_START', microtime(true));
+        $pdo = new PDO($dsn, $username, $password);
 
-/*
-|--------------------------------------------------------------------------
-| Check If The Application Is Under Maintenance
-|--------------------------------------------------------------------------
-|
-| If the application is in maintenance / demo mode via the "down" command
-| we will load this file so that any pre-rendered content can be shown
-| instead of starting the framework, which could cause an exception.
-|
-*/
 
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
-    require $maintenance;
-}
+        }catch (PDOException $e) {
+            // 接続できなかったらエラー表示
+            print('DB接続エラー'); 
+        }
+        // 部品登録テーブルの部品名の値を降順に取得して$entryに格納
+        $entry = $db->query('SELECT * FROM user ORDER BY userName DESC');
+    ?>
+<article>
+    <?php while($resister = $entry->fetch()): ?><!-- $entryの値をfetchで1件ずつ取得して$resistorへ格納 -->
+        <a href="#"><?php print(mb_substr($resister['userName'],0,50)); ?></a>
+    <?php endwhile; ?>
+</article>
+</main>
 
-/*
-|--------------------------------------------------------------------------
-| Register The Auto Loader
-|--------------------------------------------------------------------------
-|
-| Composer provides a convenient, automatically generated class loader for
-| this application. We just need to utilize it! We'll simply require it
-| into the script here so we don't need to manually load our classes.
-|
-*/
-
-require __DIR__.'/../vendor/autoload.php';
-
-/*
-|--------------------------------------------------------------------------
-| Run The Application
-|--------------------------------------------------------------------------
-|
-| Once we have the application, we can handle the incoming request using
-| the application's HTTP kernel. Then, we will send the response back
-| to this client's browser, allowing them to enjoy our application.
-|
-*/
-
-$app = require_once __DIR__.'/../bootstrap/app.php';
-
-$kernel = $app->make(Kernel::class);
-
-$response = $kernel->handle(
-    $request = Request::capture()
-)->send();
-
-$kernel->terminate($request, $response);
+</html>
