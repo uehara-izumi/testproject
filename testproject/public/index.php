111cf9513c6db42ade31423b3f0d744677e0c932
Izumin <60781486+uehara-izumi@users.noreply.github.com>
2022-05-22 01:11:42 +0900

    Add files via upload

--- testproject/public/index.php
+++ testproject/public/index.php
@@ -1,32 +1,33 @@
 <html>
 <main>
-    <h2>登録した部品名一覧</h2>
+    <h2>登録品名一した部覧</h2>
     
     <?php
+        //phpinfo();
         try{
             
-	    $dbhost = $_SERVER['RDS_HOSTNAME'];
-        $dbport = $_SERVER['RDS_PORT'];
-        $dbname = $_SERVER['RDS_DB_NAME'];
+	    $dbhost = 'database-2.ctmklwk6w2n1.ap-northeast-1.rds.amazonaws.com';
+        $dbport = '3306';
+        $dbname = 'izumiTest';
         $charset = 'utf8' ;
 
         $dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
-        $username = $_SERVER['RDS_USERNAME'];
-        $password = $_SERVER['RDS_PASSWORD'];
+        $username = 'admin';
+        $password = 'u73120491';
 
         $pdo = new PDO($dsn, $username, $password);
-
 
         }catch (PDOException $e) {
             // 接続できなかったらエラー表示
             print('DB接続エラー'); 
+            print($e);
         }
         // 部品登録テーブルの部品名の値を降順に取得して$entryに格納
-        $entry = $db->query('SELECT * FROM user ORDER BY userName DESC');
+        $entry = $pdo->query('SELECT * FROM users ORDER BY id DESC');
     ?>
 <article>
     <?php while($resister = $entry->fetch()): ?><!-- $entryの値をfetchで1件ずつ取得して$resistorへ格納 -->
-        <a href="#"><?php print(mb_substr($resister['userName'],0,50)); ?></a>
+        <a href="#"><?php print(mb_substr($resister['name'],0,50)); ?></a>
     <?php endwhile; ?>
 </article>
 </main>
